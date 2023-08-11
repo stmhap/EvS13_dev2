efcafafa23c4a084bc49806294f30c2f9704603f
stmhap <141840169+stmhap@users.noreply.github.com>
2023-08-11 13:27:01 +0530

    Updated train.py

--- train.py
+++ train.py
@@ -74,7 +74,8 @@ def main():
         #check_class_accuracy(model, train_loader, threshold=config.CONF_THRESHOLD)
 
         #if epoch > 0 and epoch % 3 == 0:
-        if epoch > 0 and epoch % 10 == 0:
+        #if epoch > 0 and epoch % 10 == 0:
+        if epoch > 0:
             check_class_accuracy(model, test_loader, threshold=config.CONF_THRESHOLD)
             pred_boxes, true_boxes = get_evaluation_bboxes(
                 test_loader,
