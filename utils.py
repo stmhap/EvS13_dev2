1dd40cee9f6ffce642b5365eb58b066c3c263187
stmhap <141840169+stmhap@users.noreply.github.com>
2023-08-12 01:37:08 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -9,6 +9,9 @@ from collections import Counter
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
+from torch_lr_finder import LRFinder
+from torchmetrics import Metric
+from torch import Tensor
 
 def iou_width_height(boxes1, boxes2):
     """
@@ -623,3 +626,18 @@ def find_lr(model, data_loader, optimizer, criterion, device=config.DEVICE):
     _, best_lr = lr_finder.plot()
     lr_finder.reset()
     return best_lr
+
+class model_accuracy(Metric):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.add_state("correct", default=torch.tensor(0), dist_reduce_fx="sum")
+        self.add_state("total", default=torch.tensor(0), dist_reduce_fx="sum")
+
+    def update(self, preds: Tensor, target: Tensor):
+        preds = preds.argmax(dim=1)
+        total = target.numel()
+        self.correct += preds.eq(target).sum()
+        self.total += total
+
+    def compute(self):
+        return 100 * self.correct.float() / self.total 
